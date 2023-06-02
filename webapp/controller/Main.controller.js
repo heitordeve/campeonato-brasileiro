7099f63bb575baa95d40ee48eeb5297b51022a13
heitor.deve <heitor.developer@outlook.com>
2023-06-01 22:14:55 -0300

    Feature - Listar dados consumindo de arquivos .json

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,14 +1,25 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, JSONModel) {
         "use strict";
 
         return Controller.extend("campeonatobrasileiro.controller.Main", {
             onInit: function () {
+                //Modelo
+                let dadosGerais = {
+                    rodada : '11ª',
+                    campeonato: 'Brasileirão 2023 do Canal FioriNET'
+                }
+
+                let dadosModel = new JSONModel()
+                dadosModel.setData(dadosGerais);
+                let view = this.getView()
+                view.setModel(dadosModel, "ModeloDadosGerais")
 
             }
         });
